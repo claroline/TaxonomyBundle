c6139d8edfbbd93b0bf77372c9f64110a8379990
Stéphane Klein <stephaneklein221@gmail.com>
2013-09-02 17:17:43 +0200

    Prepared kernel bundle integration

--- ClarolineTaxonomyBundle.php
+++ ClarolineTaxonomyBundle.php
@@ -3,7 +3,18 @@
 namespace Claroline\TaxonomyBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class ClarolineTaxonomyBundle extends Bundle
+class ClarolineTaxonomyBundle extends Bundle implements AutoConfigurableInterface
 {
+    public function supports($environment)
+    {
+        return true;
+    }
+
+    public function getConfiguration($environment)
+    {
+        return new ConfigurationBuilder();
+    }
 }
