9c3e21ee6ee3037b46a10082e45908dfb061efee
Stéphane Klein <stephaneklein221@gmail.com>
2013-09-09 11:46:26 +0200

    Removed autoconfigurable interface (not ready to be loaded in prod)

--- ClarolineTaxonomyBundle.php
+++ ClarolineTaxonomyBundle.php
@@ -3,18 +3,7 @@
 namespace Claroline\TaxonomyBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
-use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class ClarolineTaxonomyBundle extends Bundle implements AutoConfigurableInterface
+class ClarolineTaxonomyBundle
 {
-    public function supports($environment)
-    {
-        return true;
-    }
-
-    public function getConfiguration($environment)
-    {
-        return new ConfigurationBuilder();
-    }
 }
